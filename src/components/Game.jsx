9fac81c6e415b1b80604fe9e7d5d7a7309448112
NikkitoPlay <marcos.nikkito@gmail.com>
2024-02-21 14:29:11 -0400

    using states

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,35 +1,51 @@
-import './Game.css'
+import "./Game.css";
 
-const Game = ({verifyLetter}) => {
+const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
   return (
-    <div className='game'>
-      <p className='points'>
-        <span>Pontuação: 000</span>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: {score}</span>
       </p>
       <h1>Adivinhe a palavra:</h1>
-      <h3 className='tip'>
-        Dica sobre a palavra: <span> DICA</span>
+      <h3 className="tip">
+        Dica sobre a palavra: <span> {pickedCategory}</span>
       </h3>
+      <p>Você ainda tem {guesses} tentativa(s)</p>
       <div className="wordContainer">
-        <span className="letter">A</span>
-        <span className="blankSquare">
-
-        </span>
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        )}
       </div>
       <div className="letterContainer">
         <p>Tente adivinha uma letra da palavra:</p>
         <form>
-          <input type="text" name='letter' maxLength='1' required />
+          <input type="text" name="letter" maxLength="1" required />
           <button>Jogar!</button>
         </form>
       </div>
       <div className="wrongLettersContainer">
         <p>Letras já usadas: </p>
-        <span>a, </span>
-        <span>b, </span>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter}, </span>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
