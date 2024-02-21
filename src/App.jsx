9a153d29f19dcd33bd075e25cb9764febaf2a4c2
NikkitoPlay <marcos.nikkito@gmail.com>
2024-02-21 11:00:08 -0400

    navegation buttons

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,26 @@ function App() {
   const [words] = useState(wordsList);
 
   console.log(words);
+
+  //começa o jogo
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  //processa a letra
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  //recomeça o jogo
+  const retry = () => {
+     setGameStage(stages[0].name);
+  }
   return (
     <div className='App'>
-      {gameStage === 'start' && <StartScreen/>}
-      {gameStage === 'game' && <Game/>}
-      {gameStage === 'end' && <GameOver/>}
+      {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   )
 }
